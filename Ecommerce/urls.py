65311eff32932001891043811dd08871b6995437
yhon <ydenvalencia@gmail.com>
2024-10-23 20:41:10 -0500

    Separation views functions

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'Ecommerce'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
 ]
