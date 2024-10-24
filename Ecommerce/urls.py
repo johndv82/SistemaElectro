29c3d287285a0a909f7edb267e1ac87979956603
yhon <ydenvalencia@gmail.com>
2024-10-23 20:20:25 -0500

    Views layouts login/register

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -5,4 +5,6 @@ app_name = 'Ecommerce'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
 ]
