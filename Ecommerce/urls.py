a454f61bb0c1cb3b265c9b7a56f2879d722aa0f3
Luis Espetia Aroni <luisalbertoea@gmail.com>
2024-10-25 22:35:29 -0500

    imagenes en plantilla index.html

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -1,21 +1,17 @@
 from django.urls import path
 #from . import views
 from .views import cargar_categorias, listar_productos, ver_carrito, listar_pedidos
+from .views.categoria_views import index
+from Ecommerce.views import categoria_views
+from .views import categoria_views
 
-app_name = 'Ecommerce'
-
-#urlpatterns = [
-
-#    path('', views.cargar_categorias, name='index'),
-#    #path('', views.index, name='index'),
-#    path('login', views.login, name='login'),
-#    path('register', views.register, name='register'),
-
-#]
+#app_name = 'Ecommerce'
 
 urlpatterns = [
-    path('', cargar_categorias, name='index'),
+    path('', index, name='index'),
+    path('', categoria_views.index, name='index'),
     path('productos/', listar_productos, name='listar_productos'),
     path('carrito/', ver_carrito, name='ver_carrito'),
     path('pedidos/', listar_pedidos, name='listar_pedidos'),
+    path('categoria/', categoria_views.cargar_categorias, name='cargar_categorias'),
 ]
--- SistemaElectro/urls.py
+++ SistemaElectro/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# urls.py (en la raíz del proyecto)
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ecommerce/', include('Ecommerce.urls')),
+    path('', include('Ecommerce.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
